eafead1c79c224cdd57b9912f035a1cb8cf01b3f
Do Trong Hieu <hieudtr8@gmail.com>
2024-10-15 20:29:44 +0700

    fix build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from "@angular/common/http";
@@ -8,7 +8,7 @@ import { CommonModule } from "@angular/common";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
     CommonModule
   ]
