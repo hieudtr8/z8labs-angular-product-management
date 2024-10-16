c374b646384977e4178b1d13ea55c14c5b4d105c
Hieu Do <hieudtr8@gmail.com>
2024-10-16 10:18:52 +0700

    fix missing dotenv and ts-node

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -3,8 +3,6 @@ const setEnv = () => {
   const writeFile = fs.writeFile;
 // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
-// Load node modules
-  const colors = require('colors');
   require('dotenv').config({
     path: 'src/environments/.env'
   });
@@ -13,13 +11,13 @@ const setEnv = () => {
     apiUrl: '${process.env['API_URL']}',
     production: true,
   };`;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  console.log('The file `environment.ts` will be written with the following content: \n');
   writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
     }
   });
 };
