a1c2bbabb51f09970ad34b8dbab053c5e313fe99
Hieu Do <hieudtr8@gmail.com>
2024-10-18 13:00:45 +0700

    fix env

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,4 +1,13 @@
 export const environment = {
-    apiUrl: 'https://670e32a8073307b4ee45da4b.mockapi.io/angular-hieudtr8-product-management',
-    production: true,
-  };
+  production: true,
+  apiUrl: 'https://670e32a8073307b4ee45da4b.mockapi.io/angular-hieudtr8-product-management',
+  firebase: {
+    projectId: 'hieudt-z8labs-angular-product',
+    appId: '1:281263639773:web:d81c1c12b73397a32c5f34',
+    storageBucket: 'hieudt-z8labs-angular-product.appspot.com',
+    apiKey: 'AIzaSyBvRBLMybFt3uZKINXtla-uETaJuxrWLOg',
+    authDomain: 'hieudt-z8labs-angular-product.firebaseapp.com',
+    messagingSenderId: '281263639773',
+    measurementId: 'G-1NL90Y4D5J'
+  }
+};
--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -9,6 +9,15 @@ const setEnv = () => {
 // `environment.ts` file structure
   const envConfigFile = `export const environment = {
     apiUrl: '${process.env['API_URL']}',
+    firebase: {
+      projectId: '${process.env['FIREBASE_PROJECT_ID']}',
+      appId: '${process.env['FIREBASE_APP_ID']}',
+      storageBucket: '${process.env['FIREBASE_STORAGE_BUCKET']}',
+      apiKey: '${process.env['FIREBASE_API_KEY']}',
+      authDomain: '${process.env['FIREBASE_AUTH_DOMAIN']}',
+      messagingSenderId: '${process.env['FIREBASE_MESSAGING_SENDER_ID']}',
+      measurementId: '${process.env['FIREBASE_MEASUREMENT_ID']}'
+    },
     production: true,
   };`;
   console.log('The file `environment.ts` will be written with the following content: \n');
